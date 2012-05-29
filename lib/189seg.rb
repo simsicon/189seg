eb7625a9873e22841f49c64f232daa0a68f3f1ff
simsicon <simsicon@gmail.com>
2012-05-29 17:28:02 +0800

    require

--- lib/189seg.rb
+++ lib/189seg.rb
@@ -1,5 +1,16 @@
 require "189seg/version"
+require 'openssl'
+require 'base64'
+require 'rest_client'
+require 'yajl'
+require 'net/http'
+require 'uri'
+require 'json'
+
 
 module CnTelecomeSeg
-  # Your code goes here...
+  
 end
+
+directory = File.expand_path(File.dirname(__FILE__))
+require File.join(directory, '189seg', 'base')
